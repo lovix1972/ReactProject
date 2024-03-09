ddeb13c14ae1110b0c9c43e3fd61fd08b452701d
pasquale.lovicu <Pasquale Lovicu>
2024-03-09 12:40:53 +0100

    Prova Modifica progetto React

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        prova
         </p>
         <a
           className="App-link"
